ffc97edf18a66c14f6aae43558723224b2acef50
sramirezc <checo7_10@hotmail.com>
2012-11-28 21:37:11 -0600

    Podemos intentar abordar el problema desde un hola mundo propio en C y su equivalente en java listo para Android.

---
+++ holaMundo.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SumaRecursiva
+//
+//  Created by Sergio Ramírez on 01/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+    printf("Hello world!");
+    return 0;
+}
